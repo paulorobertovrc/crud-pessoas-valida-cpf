f71cddf838b2bc65e3c30762c46702eee7d80d6a
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-11-21 09:35:32 -0400

    Refatoração da classe e sobrescrita do método ToString.

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,18 +1,23 @@
 public class Pessoa {
-    private static int id = 0;
+    private static int totalDePessoas = 0;
+    
     private String nome;
     private Cpf cpf;
-    private String sequencia;
+    private int id;
         
     public Pessoa(String nome, String cpf) {
         this.nome = nome;
         this.cpf = new Cpf(cpf);
-        id++;
-        this.sequencia = String.valueOf(id);
+        totalDePessoas++;
+        this.id = totalDePessoas;
     }
 
-    public static int getId() {
-        return id;
+    public static int getTotalDePessoas() {
+        return totalDePessoas;
+    }
+
+    public static void setTotalPessoas(int numero) {
+        Pessoa.totalDePessoas = numero;
     }
 
     public String getNome() {
@@ -23,7 +28,12 @@ public class Pessoa {
         return cpf;
     }
 
-    public String getSequencia() {
-        return sequencia;
-    }    
+    public int getId() {
+        return id;
+    }
+    
+    @Override
+    public String toString() {
+        return "ID: " + this.id +  "| Nome: " + this.nome + " | CPF: " + this.cpf;
+    }
 }
