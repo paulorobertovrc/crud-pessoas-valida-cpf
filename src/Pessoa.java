ed74f73c3868aa3b4f57ce440529986d62fb69ce
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-11-21 09:52:54 -0400

    Adição de comentários

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,8 +1,10 @@
 public class Pessoa {
+    // atributo estático para controlar o total de pessoas cadastradas
     private static int totalDePessoas = 0;
     
     private String nome;
     private Cpf cpf;
+    // identificador único de cada pessoa
     private int id;
         
     public Pessoa(String nome, String cpf) {
@@ -12,10 +14,13 @@ public class Pessoa {
         this.id = totalDePessoas;
     }
 
+    // Retorna o total de pessoas cadastradas
     public static int getTotalDePessoas() {
         return totalDePessoas;
     }
 
+    // Este setter foi criado para permitir a redução do total de cadastros após a exclusão de uma pessoa do banco
+    // Os métodos de manipulação do banco estão na classe Banco
     public static void setTotalPessoas(int numero) {
         Pessoa.totalDePessoas = numero;
     }
@@ -34,6 +39,6 @@ public class Pessoa {
     
     @Override
     public String toString() {
-        return "ID: " + this.id +  "| Nome: " + this.nome + " | CPF: " + this.cpf;
+        return "ID: " + this.id +  " | Nome: " + this.nome + " | CPF: " + this.cpf;
     }
 }
