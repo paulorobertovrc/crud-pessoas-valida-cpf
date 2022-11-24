cade345700d38fb5cbb270c50043b0ce39d8efdc
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-11-23 21:57:26 -0400

    Implementação do menu principal da aplicação

--- src/Menu.java
+++ src/Menu.java
@@ -2,42 +2,96 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Menu {
-    
+
+    // static boolean continuar = true;
+
     public static void imprimirMenu() {
+        // apagarConsole();
         System.out.println("============== MENU PRINCIPAL ==============");
         System.out.println("[ 1 ] CADASTRAR");
         System.out.println("[ 2 ] BUSCAR");
         System.out.println("[ 3 ] ATUALIZAR");
         System.out.println("[ 4 ] EXCLUIR");
         System.out.println("[ 5 ] SAIR");
-        System.out.println("Selecione a opção desejada:");
-        System.out.print("> ");
+        System.out.print("Selecione a opção desejada: ");
         
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = Main.getScanner().useDelimiter("\n");
         int opcaoSelecionada = sc.nextInt();
         switch (opcaoSelecionada) {
             case 1:
-                System.out.println("op 1");
+                apagarConsole();
+                System.out.println("============== CADASTRAR PESSOA ==============");
+                System.out.print("Nome: ");
+                String nome = sc.next();
+                System.out.print("CPF: ");
+                String cpf = sc.next();
+                System.out.println();
+                new Create(nome, cpf);
+                System.out.println(nome + " cadastrado(a) com sucesso.");
+                System.out.println();
+                System.out.println("Total de pessoas cadastradas: " + Pessoa.getTotalDePessoas());
+                prosseguir();
                 break;
             case 2:
-                System.out.println("op 2");
+                apagarConsole();
+                System.out.println("============== BUSCAR PESSOA ==============");
+                System.out.print("Informe o ID do cadastro para busca [informe 0 para listar todos]: ");
+                int respostaCase2 = sc.nextInt();
+                System.out.println();
+                
+                if (respostaCase2 == 0) {
+                    new Read();
+                } else {
+                    new Read(respostaCase2);
+                }
+
+                System.out.println();
+                prosseguir();
                 break;
             case 3:
-                System.out.println("op 3");
+                apagarConsole();
+                System.out.println("============== ATUALIZAR PESSOA ==============");
+                System.out.print("Informe o ID do cadastro a ser atualizado: ");
+                int respostaCase3 = sc.nextInt();
+                System.out.print("Informe o novo nome: ");
+                String novoNome = sc.next();
+                new Update(respostaCase3, novoNome);
+                System.out.println();
+                System.out.println(Banco.buscar(respostaCase3));
+                System.out.println();
+                prosseguir();
                 break;
             case 4:
-                System.out.println("op 4");
+                apagarConsole();
+                System.out.println("============== EXCLUIR PESSOA ==============");
+                System.out.print("Informe o ID do cadastro a ser excluído: ");
+                int respostaCase4 = sc.nextInt();
+                new Delete(respostaCase4);
+                System.out.println();
+                System.out.println("Cadastro ID " + respostaCase4 + " excluído com sucesso.");
+                System.out.println("Total de pessoas cadastradas: " + Pessoa.getTotalDePessoas());
+                System.out.println();
+                prosseguir();
                 break;
             case 5:
-                System.out.println("op 5");
+                Main.finalizar();
                 break;
         
             default:
                 System.out.println("Opção inválida. Tente novamente.");
+                System.out.println();
+                imprimirMenu();
         }
-        // sc.close();
+    }
 
-        System.out.println(opcaoSelecionada);
+    private static void prosseguir() {
+        try {
+            System.out.println();
+            System.out.print("Pressione [ENTER] para prosseguir ... ");
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void apagarConsole() {
